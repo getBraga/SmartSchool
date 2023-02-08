6c990608b5a8a305f8f93cd876159052a014e223
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-08 00:14:34 -0300

    primeiro commit

---
+++ SmartSchool.WebAPI/Models/AlunoDisciplina.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartSchool.WebAPI.Models
+{
+    public class AlunoDisciplina
+    {
+        public AlunoDisciplina() { }
+
+        public AlunoDisciplina(int alunoID, int disciplinaId)
+        {
+            AlunoID = alunoID;
+            DisciplinaId = disciplinaId;
+        }
+
+        public int AlunoID { get; set; }
+        public Aluno Aluno { get; set; }
+        public Disciplina Disciplina { get; set; }
+        public int DisciplinaId { get; set; }
+    }
+}
---
+++ SmartSchool.WebAPI/Models/Professor.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartSchool.WebAPI.Models
+{
+    public class Professor
+    {
+        public Professor() { }
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
+    }
+}
