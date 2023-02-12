9b021d791cd0dff5148c25fb99c84969f7cb0e95
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-12 00:11:07 -0300

    adicionado o DataContext para o projeto

---
+++ SmartSchool.WebAPI/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Models;
+
+namespace SmartSchool.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) {}
+      
+        public DbSet <Aluno> Alunos{ get; set; }
+        public DbSet<Professor> Professor { get; set; }
+        public DbSet<Disciplina> Disciplina { get; set; }
+        public DbSet<AlunoDisciplina> AlunoDisciplina { get; set; }
+       protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AlunoDisciplina>()
+                .HasKey(AD => new { AD.AlunoID, AD.DisciplinaId });
+        }
+    }
+}
