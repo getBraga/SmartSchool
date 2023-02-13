3ac1fb91fc4bc80a8e8dc2ea7b23ff7a259b2471
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-13 02:04:40 -0300

    salvando os dados no banco

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SmartSchool.WebAPI.Data;
 using SmartSchool.WebAPI.Models;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -43,22 +44,38 @@ namespace SmartSchool.WebAPI.Controllers
         [HttpPost]
         public IActionResult PostAluno(Aluno aluno)
         {
+            _context.Add(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
 
         [HttpPut("{id}")]
         public IActionResult PutAluno(int id, Aluno aluno) 
         {
+            var findAluno = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (findAluno == null) return BadRequest("O Aluno não foi encontrado!");
+
+            _context.Update(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
         [HttpPatch("{id}")]
         public IActionResult PatchAluno(int id, Aluno aluno)
         {
+            var findAluno = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (findAluno == null) return BadRequest("O Aluno não foi encontrado!");
+            _context.Update(aluno);
+            _context.SaveChanges();
             return Ok(aluno);
         }
         [HttpDelete("{id}")]
         public IActionResult DeleteAluno(int id)
         {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("O Aluno não foi encontrado!");
+
+            _context.Remove(aluno);
+            _context.SaveChanges();
             return Ok("Usuário excluido");
         }
     }
