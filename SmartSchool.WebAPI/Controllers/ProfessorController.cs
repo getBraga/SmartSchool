92ad8d2823781e80cd28cb63c6a380ec70ebf952
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-16 01:15:22 -0300

    trocado a propriedade professor para professores

--- SmartSchool.WebAPI/Controllers/ProfessorController.cs
+++ SmartSchool.WebAPI/Controllers/ProfessorController.cs
@@ -22,12 +22,12 @@ namespace SmartSchool.WebAPI.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_context.Professor);
+            return Ok(_context.Professores);
         }
         [HttpGet("byId/{id}")]
         public IActionResult GetId(int id)
         {
-            var professor = _context.Professor.FirstOrDefault(p => p.Id == id);
+            var professor = _context.Professores.FirstOrDefault(p => p.Id == id);
             if (professor == null) return BadRequest("O Professor não foi encontrado!");
             return Ok(professor);
         }
@@ -35,7 +35,7 @@ namespace SmartSchool.WebAPI.Controllers
         [HttpGet("ByName")]
         public IActionResult ByName(string name)
         {
-            var professor = _context.Professor.FirstOrDefault(p => p.Nome == name);
+            var professor = _context.Professores.FirstOrDefault(p => p.Nome == name);
             if (professor == null) return BadRequest("O Professor não foi encontrado!");
             return Ok(professor);
         }
@@ -54,7 +54,7 @@ namespace SmartSchool.WebAPI.Controllers
         [HttpPut("{id}")]
         public IActionResult PutProfessor(int id, Professor professor)
         {
-            var findProfessor = _context.Professor.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            var findProfessor = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
             if (findProfessor == null) return BadRequest("O Professor não foi encontrado!");
             _context.Update(professor);
             _context.SaveChanges();
@@ -63,7 +63,7 @@ namespace SmartSchool.WebAPI.Controllers
         [HttpPatch("{id}")]
         public IActionResult PatchProfessor(int id, Professor professor)
         {
-            var findProfessor = _context.Professor.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            var findProfessor = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
             if (findProfessor == null) return BadRequest("O Professor não foi encontrado!");
             _context.Update(professor);
             _context.SaveChanges();
@@ -73,7 +73,7 @@ namespace SmartSchool.WebAPI.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteProfessor(int id)
         {
-            var professor = _context.Professor.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            var professor = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
             if (professor == null) return BadRequest("O Professor não foi encontrado!");
             _context.Remove(professor);
             _context.SaveChanges();
