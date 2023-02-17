27eee9aed7998b1cffa23279851f8246481a59e7
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-17 01:06:08 -0300

    adicionado novas propriedades

--- SmartSchool.WebAPI/Models/AlunoDisciplina.cs
+++ SmartSchool.WebAPI/Models/AlunoDisciplina.cs
@@ -9,7 +9,9 @@
             AlunoId = alunoId;
             DisciplinaId = disciplinaId;
         }
-
+        public DateTime DataInicio { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; } = null;
+        public int? Nota { get; set; } = null;
         public int AlunoId { get; set; }
         public Aluno Aluno { get; set; }
         public Disciplina Disciplina { get; set; }
