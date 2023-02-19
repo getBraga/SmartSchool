14d1429b629c18250fda170a67bc24db9a7c8e21
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-19 03:04:16 -0300

    adicionado a data de nascimento do professor

--- SmartSchool.WebAPI/Models/Professor.cs
+++ SmartSchool.WebAPI/Models/Professor.cs
@@ -4,12 +4,17 @@
     {
         public Professor() { }
 
-        public Professor(int id, int registro, string nome, string sobrenome)
+        public Professor(int id, 
+                         int registro, 
+                         string nome, 
+                         string sobrenome, 
+                         DateTime dataNascimento)
         {
             Id = id;
             Registro = registro;
             Nome = nome;
             Sobrenome = sobrenome;
+            DataNascimento = dataNascimento; 
         }
 
         public int Id { get; set; }
@@ -18,6 +23,7 @@
         public string Sobrenome { get; set; }
         public DateTime DataInicio { get; set; } = DateTime.Now;
         public DateTime? DataFim { get; set; } = null;
+        public DateTime DataNascimento { get; set; }
         public bool Ativo { get; set; } = true;
         public IEnumerable<Disciplina> Disciplinas { get; set; }
     }
