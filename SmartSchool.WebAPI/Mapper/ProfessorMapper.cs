e1d66546766a6ae37a271b7ff51ca80f3990d6f1
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-19 03:04:27 -0300

    professormapper

---
+++ SmartSchool.WebAPI/Mapper/ProfessorMapper.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using SmartSchool.WebAPI.Dtos;
+using SmartSchool.WebAPI.Helpers;
+using SmartSchool.WebAPI.Models;
+
+using AutoMapper;
+using SmartSchool.WebAPI.Dtos;
+using SmartSchool.WebAPI.Models;
+
+namespace SmartSchool.WebAPI.Mapper
+{
+    public class ProfessorMapper : Profile
+    {
+        public ProfessorMapper()
+        {
+            CreateMap<Professor, ProfessorDto>()
+           .ForMember(
+               dest => dest.Nome,
+               opt => opt.MapFrom(nomeSobrenome => $"{nomeSobrenome.Nome} {nomeSobrenome.Sobrenome}")
+           )
+           .ForMember(
+                dest => dest.Idade,
+                opt => opt.MapFrom(idade => idade.DataNascimento.GetCurrentAge())
+                );
+
+
+            CreateMap<ProfessorDto, Professor>();
+            CreateMap<Professor, ProfessorRegistrarDto>().ReverseMap();
+        }
+    }
+}
