8b1a3877a9b2e370128ec7daa4f8d74000e55c7a
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-19 00:14:18 -0300

    iniciado o alunoDto para ser mapeado

---
+++ SmartSchool.WebAPI/Dtos/AlunoDto.cs
@@ -0,0 +1,16 @@
+﻿using SmartSchool.WebAPI.Models;
+
+namespace SmartSchool.WebAPI.Dtos
+{
+    public class AlunoDto
+    {
+        public int Id { get; set; }
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataInicio { get; set; } = DateTime.Now;
+        public bool Ativo { get; set; } 
+    }
+}
