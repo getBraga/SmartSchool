19ee0591773ace96832762a57f4641bc74e49070
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-19 00:15:21 -0300

    mappeamento do aluno junto com o alunoDto

---
+++ SmartSchool.WebAPI/Mapper/SmartSchoolProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using SmartSchool.WebAPI.Dtos;
+using SmartSchool.WebAPI.Models;
+
+namespace SmartSchool.WebAPI.Helpers
+{
+    public class SmartSchoolProfile:Profile
+    {
+        public SmartSchoolProfile()
+        {
+            CreateMap<Aluno, AlunoDto>()
+                .ForMember(
+                dest => dest.Nome,
+                opt => opt.MapFrom(nomeSobrenome => $"{nomeSobrenome.Nome} {nomeSobrenome.Sobrenome}")
+                )
+                .ForMember(
+                dest => dest.Idade,
+                opt => opt.MapFrom(idade => idade.DataNascimento.GetCurrentAge())
+            );
+
+        }
+    }
+}
