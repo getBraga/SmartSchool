f2869c24204eaf3702bed4ab7beac544d4db77a9
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-20 01:53:59 -0300

    dto para atualizar o professor

---
+++ SmartSchool.WebAPI/Dtos/ProfessorAtualizarDto.cs
@@ -0,0 +1,47 @@
+﻿namespace SmartSchool.WebAPI.Dtos
+{
+    public class ProfessorAtualizarDto
+    {
+        /// <summary>
+        /// Identificador Unico no banco de dados
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nome do professor
+        /// </summary>
+        public int Registro { get; set; }
+        /// <summary>
+        /// Nome completo do professor
+        /// </summary>
+        public string? NomeCompleto { get; set; }
+
+        /// <summary>
+        /// Nome do professor
+        /// </summary>
+        public string Nome { get; set; }
+        /// <summary>
+        /// Sobrenome do professor
+        /// </summary>
+        public string? Sobrenome { get; set; }
+        /// <summary>
+        /// Telefone do professor
+        /// </summary>
+        public string? Telefone { get; set; }
+        /// <summary>
+        /// Data de inicio na smartScool
+        /// </summary>
+        public DateTime DataInicio { get; set; } = DateTime.Now;
+        /// <summary>
+        /// Data de nascimento do Professor
+        /// </summary>
+        public DateTime DataNascimento { get; set; }
+        /// <summary>
+        /// Data do termino de contrato na SmartSchool
+        /// </summary>
+        public DateTime? DataFim { get; set; } = null;
+        /// <summary>
+        /// Verificacao se o professor está ativo
+        /// </summary>
+        public bool Ativo { get; set; } = true;
+    }
+}
