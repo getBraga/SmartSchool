cc629d3ac88f5c278392bda187296b0e65db9963
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-20 01:53:16 -0300

    feito a chamada do alunoatualizar dto e inserido comentario nos metodos para o swagger

--- SmartSchool.WebAPI/Controllers/AlunoController.cs
+++ SmartSchool.WebAPI/Controllers/AlunoController.cs
@@ -8,6 +8,9 @@ using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class AlunoController : ControllerBase
@@ -15,12 +18,21 @@ namespace SmartSchool.WebAPI.Controllers
         private readonly IMapper _mapper;
 
         private  IRepository _repo { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repo"></param>
+        /// <param name="mapper"></param>
         public AlunoController(IRepository repo, IMapper mapper)
         {
             this._repo = repo;
             this._mapper = mapper;
         }
 
+        /// <summary>
+        /// Metodo responsável por retornar todos os alunos
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Get()
         {
@@ -28,6 +40,11 @@ namespace SmartSchool.WebAPI.Controllers
             var alunoDto =  _mapper.Map<IEnumerable<AlunoDto>>(alunos);
             return Ok(alunoDto);
         }
+        /// <summary>
+        /// Metodo responsável por retornar um unico aluno por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public IActionResult GetId(int id)
         {
@@ -38,7 +55,11 @@ namespace SmartSchool.WebAPI.Controllers
             return Ok(alunoDto);
 
         }
-
+        /// <summary>
+        /// Metodo responsável por registrar um aluno na base de dados
+        /// </summary>
+        /// <param name="alunoDto"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult PostAluno(AlunoRegistrarDto alunoDto)
         {
@@ -46,13 +67,19 @@ namespace SmartSchool.WebAPI.Controllers
             _repo.Add(aluno);
             if(_repo.SaveChanges())
             {
-                return Created($"/api/aluno/${alunoDto.Id}", _mapper.Map<AlunoDto>(aluno));
+                return Created($"/api/aluno/${aluno.Id}", _mapper.Map<AlunoDto>(aluno));
             }
             return BadRequest("Aluno não cadastrado!");
         }
 
+        /// <summary>
+        /// Metodo responsável por atualizar o aluno na base de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
-        public IActionResult PutAluno(int id, AlunoRegistrarDto model) 
+        public IActionResult PutAluno(int id, AlunoAtualizarDtro model) 
         {
             var aluno = _repo.GetAlunoById(id, false);
             if (aluno == null) return BadRequest("O Aluno não foi encontrado!");
@@ -60,14 +87,20 @@ namespace SmartSchool.WebAPI.Controllers
             _repo.Update(aluno);
             if (_repo.SaveChanges())
             {
-                return Created($"/api/aluno/${model.Id}", _mapper.Map<AlunoDto>(aluno));
+                return Created($"/api/aluno/${model.Id}", _mapper.Map<AlunoAtualizarDtro>(aluno));
 
             }
             return BadRequest("O Aluno não foi encontrado!");
           
         }
+        /// <summary>
+        ///  Metodo responsável por atualizar o aluno na base de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPatch("{id}")]
-        public IActionResult PatchAluno(int id, AlunoRegistrarDto model)
+        public IActionResult PatchAluno(int id, AlunoAtualizarDtro model)
         {
             var aluno = _repo.GetAlunoById(id, false);
             if (aluno == null) return BadRequest("O Aluno não foi encontrado!");
@@ -76,11 +109,16 @@ namespace SmartSchool.WebAPI.Controllers
             _repo.Update(aluno);
             if (_repo.SaveChanges())
             {
-                return Created($"/api/aluno/${model.Id}", _mapper.Map<AlunoDto>(aluno));
+                return Created($"/api/aluno/${model.Id}", _mapper.Map<AlunoAtualizarDtro>(aluno));
 
             }
             return BadRequest("O Aluno não foi encontrado!");
         }
+        /// <summary>
+        ///  Metodo responsável por remover o aluno na base de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public IActionResult DeleteAluno(int id)
         {
@@ -90,9 +128,9 @@ namespace SmartSchool.WebAPI.Controllers
             _repo.Delete(aluno);
             if (_repo.SaveChanges())
             {
-                return Ok("Usuário excluido");
+                return Ok("Usuário deletado com sucesso!");
             }
-            return BadRequest("Usuário não excluido");
+            return BadRequest("Usuário não deletado");
            
         }
     }
