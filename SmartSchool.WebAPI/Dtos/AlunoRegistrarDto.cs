e7252fa74121b89fbebce1adb660030668de1da9
Ysmael Braga <ysmael.braga@ibm.com>
2023-02-20 01:55:09 -0300

    inserido comentarios no dtos e retirado o campo id para o registro na base de dados

--- SmartSchool.WebAPI/Dtos/AlunoRegistrarDto.cs
+++ SmartSchool.WebAPI/Dtos/AlunoRegistrarDto.cs
@@ -4,14 +4,38 @@ namespace SmartSchool.WebAPI.Dtos
 {
     public class AlunoRegistrarDto
     {
-        public int Id { get; set; }
+
+        /// <summary>
+        /// Matricula do aluno
+        /// </summary>
         public int Matricula { get; set; }
+        /// <summary>
+        /// Nome do aluno
+        /// </summary>
         public string Nome { get; set; }
+        /// <summary>
+        /// Sobrenome do aluno
+        /// </summary>
         public string? Sobrenome { get; set; }
+        /// <summary>
+        /// Telefone do aluno
+        /// </summary>
         public string? Telefone { get; set; }
+        /// <summary>
+        /// Data de nascimento do aluno
+        /// </summary>
         public DateTime DataNascimento { get; set; }
+        /// <summary>
+        /// Data de entrada na SmartSchool
+        /// </summary>
         public DateTime DataInicio { get; set; } = DateTime.Now;
+        /// <summary>
+        /// Data fim  na SmartSchool
+        /// </summary>
         public DateTime? DataFim { get; set; } = null;
+        /// <summary>
+        /// Verifica se o aluno está ativo
+        /// </summary>
         public bool Ativo { get; set; } = true;
     }
 }
--- SmartSchool.WebAPI/Dtos/ProfessorRegistrarDto.cs
+++ SmartSchool.WebAPI/Dtos/ProfessorRegistrarDto.cs
@@ -4,14 +4,38 @@ namespace SmartSchool.WebAPI.Dtos
 {
     public class ProfessorRegistrarDto
     {
-        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do professor
+        /// </summary>
         public int Registro { get; set; }
+        /// <summary>
+        /// Nome do professor
+        /// </summary>
         public string Nome { get; set; }
+        /// <summary>
+        /// Sobrenome do professor
+        /// </summary>
         public string Sobrenome { get; set; }
-        public string Telefone { get; set; }
+        /// <summary>
+        /// Telefone do professor
+        /// </summary>
+        public string? Telefone { get; set; }
+        /// <summary>
+        /// Data de inicio na smartScool
+        /// </summary>
         public DateTime DataInicio { get; set; } = DateTime.Now;
+        /// <summary>
+        /// Data de nascimento do Professor
+        /// </summary>
         public DateTime DataNascimento { get; set; }
+        /// <summary>
+        /// Data do termino de contrato na SmartSchool
+        /// </summary>
         public DateTime? DataFim { get; set; } = null;
+        /// <summary>
+        /// Verificacao se o professor está ativo
+        /// </summary>
         public bool Ativo { get; set; } = true;
     }
 }
